5f0ec485c4c0340e938935312deae600e1e81653
Razita <razita.afrina@gmail.com>
2024-03-21 17:48:34 +0700

    add dot to logo

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ const Welcome = () => {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-neutral-50 bg-[url('../public/background.svg')]">
           <div className='justify-center items-center flex flex-col w-fit'>
-            <div className='flex flex-wrap justify-center items-center mb-3'>
-              <h1 className='font-bold text-6xl md:text-8xl font-serif text-center pb-6 mx-4 text-blue-950'>Welcome to</h1>
-              <img src='logo-long.svg' alt='Thriftin' className='h-20 md:h-28'></img>
+            <div className='flex flex-wrap justify-center items-center mb-6'>
+              <h1 className='font-bold text-6xl md:text-8xl font-serif text-center mx-4 text-blue-950'>Welcome to</h1>
+              <img src='logo-long.svg' alt='Thriftin' className='h-20 md:h-32'></img>
             </div>
             <h2 className='font-bold text-xl md:text-2xl font-bold text-center pb-6 mx-4 px-4 sm:px-10 lg:px-0 text-blue-950'>Ubah koleksi pakaianmu menjadi lebih unik, dengan cara yang ramah lingkungan!</h2>
             <Link href="/order"><button className='px-8 py-4 rounded-xl w-fit bg-blue-950 text-neutral-50 text-xl md:text-2xl font-bold' onClick={() => {}}>Mulai</button></Link>
